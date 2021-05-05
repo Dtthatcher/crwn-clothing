b6b47861d17f301c6e999911d76b2b5525f4ad3f
David Thatcher <david@thatchermethod.com>
2021-05-05 14:38:57 -0400

    menu items added and styled

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './App.css';
-import HomePage from './homepage.component'
+import HomePage from './pages/homepage/hompage.component'
 
 function App() {
   return (
