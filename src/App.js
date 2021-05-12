f4eeb27a9c31a3f8b7483b36cb9d54e017a41cdf
David Thatcher <david@thatchermethod.com>
2021-05-12 13:06:47 -0400

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/hompage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import Header from './components/header/header.component'
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 class App extends React.Component {
   constructor() {
@@ -21,9 +21,23 @@ class App extends React.Component {
   unsubscripeFromAuth = null
 
   componentDidMount() {
-    this.unsubscripeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-    })
+    this.unsubscripeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth){
+        const userRef= await createUserProfileDocument(userAuth)
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        });        
+      }
+      else {
+        this.setState({ currentUser: userAuth })
+      }
+    });
   }
 
   componentWillUnmount() {
