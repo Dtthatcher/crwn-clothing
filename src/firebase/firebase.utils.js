d23a863bdc1692323c511412fd3c8b6cb820cddf
David Thatcher <david@thatchermethod.com>
2021-05-26 11:51:21 -0400

    implement code that pulls in our shop data from firestore and stores it into the shop reducer from our shop page. also included our withSpinner HOC

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -40,6 +40,39 @@ const config = {
 
   firebase.initializeApp(config);
 
+  export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+      const collectionRef = firestore.collection(collectionKey);
+      console.log(collectionRef);
+
+      const batch = firestore.batch();
+      objectsToAdd.forEach(obj => {
+          const newDocRef = collectionRef.doc();
+          batch.set(newDocRef, obj);
+      })
+
+      await batch.commit()
+  }
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+      const transformedCollection = collections.docs.map(
+          doc =>{
+              const { title, items } = doc.data();
+
+              return {
+                  routeName: encodeURI(title.toLowerCase()),
+                  id: doc.id,
+                  title,
+                  items
+              }
+          }
+      );
+
+      return transformedCollection.reduce((acc, collection) => {
+          acc[collection.title.toLowerCase()] = collection;
+          return acc;
+      }, {});
+  }
+
   export const auth = firebase.auth()
   export const firestore = firebase.firestore();
 
